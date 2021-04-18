c2fed39b1ba63aa73f96239cba0c460b2de23108
Piraveen Mahesan <mpiraveen95@yahoo.com>
2021-04-18 22:48:21 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/rocketreserver/LoginFragment.kt
@@ -0,0 +1,56 @@
+package com.example.rocketreserver
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.apollographql.apollo.api.Input
+import com.apollographql.apollo.coroutines.await
+import com.apollographql.apollo.exception.ApolloException
+import com.example.rocketreserver.databinding.LoginFragmentBinding
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: LoginFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = LoginFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    @SuppressLint("LongLogTag")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.submitProgressBar.visibility = View.GONE
+        binding.submit.setOnClickListener() {
+            val email = binding.email.text.toString()
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.emailLayout.error = getString(R.string.invalid_email)
+                return@setOnClickListener
+            }
+
+            binding.submitProgressBar.visibility = View.VISIBLE
+            binding.submit.visibility = View.GONE
+                lifecycleScope.launchWhenResumed {
+                    var response = try {
+                        apolloClient.mutate(LoginMutation(email = Input.fromNullable(email))).await()
+                    } catch (e: ApolloException) {
+                        Log.i("Error on Click Submitted : ", e.toString())
+                    }
+                }
+
+
+
+
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/rocketreserver/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.rocketreserver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rocketreserver.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
